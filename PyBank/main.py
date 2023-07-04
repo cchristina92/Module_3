7beac1966b34053f45b351de2b6e8b74ebfacc88
cchristina92 <130312075+cchristina92@users.noreply.github.com>
2023-07-03 23:12:41 -0700

    PyBank final edit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,23 @@
-import csv
+import csv 
 import os
 
-# Set file path 
-file_path = os.path.join("..", "Resources", "budget_data.csv")
-
-# Set text output
-text_path = os.path.join(".", "analysis", "analysis_results.txt")
+# Set the file path and text output
+file_path = os.path.join("Resources", "budget_data.csv")
+text_path = os.path.join("analysis", "analysis_results.txt")
 
 # Variables
 total_months = 0
 net_total = 0
 prev_profit_loss = 0
 total_changes = 0
-greatest_increase = 0
-greatest_increase_month = ""
-greatest_decrease = 0
-greatest_decrease_month = ""
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999]
 
 # Read the CSV file
-with open(file_path, "r") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+with open(file_path) as csvfile:
+    csvreader = csv.reader(csvfile)
     
-    # Header row
+    # Skip the header
     header = next(csvreader)
     
     # Loop through each row 
@@ -32,41 +28,47 @@ with open(file_path, "r") as csvfile:
         # Get the profit/loss value for the current row
         current_profit_loss = int(row[1])
         
-        # Calculate net total
+        # Calculate the net total
         net_total += current_profit_loss
         
-        # Calculate change in profit/loss
+        # Calculate the change in profit/loss since the previous row
         if total_months > 1:
             change = current_profit_loss - prev_profit_loss
             total_changes += change
             
             # Check if the change is the greatest increase or decrease
-            if change > greatest_increase:
-                greatest_increase = change
-                greatest_increase_month = row[0]
-            if change < greatest_decrease:
-                greatest_decrease = change
-                greatest_decrease_month = row[0]
+            if change > greatest_increase[1]:
+                greatest_increase[1] = change
+                greatest_increase[0] = row[0]
+            if change < greatest_decrease[1]:
+                greatest_decrease[1] = change
+                greatest_decrease[0] = row[0]
         
-        
+        # Set the current profit/loss as the previous profit/loss for the next iteration
         prev_profit_loss = current_profit_loss
 
-# Calculate average change
+# Calculate the average change
 average_change = total_changes / (total_months - 1)
 
-# Output Summary
+# Print the analysis
+print("Financial Analysis")
+print("-------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_total}")
+print(f"Average Change: ${average_change:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase}")
+print(f"Greatest Decrease in Profits: {greatest_decrease} ")
+
 output = (
     f"Financial Analysis\n"
     f"----------------------------\n"
     f"Total Months: {total_months}\n"
     f"Total: ${net_total}\n"
     f"Average Change: ${average_change:.2f}\n"
-    f"Greatest Increase in Profits: {greatest_increase_month[0]} (${greatest_increase[1]})\n"
-    f"Greatest Decrease in Profits: {greatest_decrease_month[0]} (${greatest_decrease[1]})\n")
+    f"Greatest Increase in Profits: {greatest_increase[1]}\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[1]}\n")
 
-print(output)
 
 # Export to text file
 with open(text_path, "w") as txt_file:
     txt_file.write(output)
-
